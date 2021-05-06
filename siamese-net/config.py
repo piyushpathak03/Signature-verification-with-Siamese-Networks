b1c98588c11e4d366fe622373827e8653abfe077
Piyush Pathak <57284769+piyushpathak03@users.noreply.github.com>
2021-05-06 13:07:03 +0530

    Add files via upload

---
+++ siamese-net/config.py
@@ -0,0 +1,6 @@
+training_dir = "C:/Users/piyush.pathak/Downloads/siamese_net-master/siamese_net-master/train"
+training_csv = "C:/Users/piyush.pathak/Downloads/siamese_net-master/siamese_net-master/train_data.csv"
+testing_csv = "C:/Users/piyush.pathak/Downloads/siamese_net-master/siamese_net-master/test_data.csv"
+testing_dir = "C:/Users/piyush.pathak/Downloads/siamese_net-master/siamese_net-master/test"
+batch_size = 32
+epochs = 20
